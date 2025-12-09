9a3fc9e68b9a38a66edc22880e42f7f39078c8d7
Resham <resha@MacBook-Air-2.local>
2025-12-09 14:30:36 -0800

    Calculating movies average rating

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
     ifstream file("210-xc2-FA25.txt");
     string title;
     int rating;
+    double averageRating = 0.0;
 
     map<string, vector<int>> movies;
 
@@ -22,12 +23,16 @@ int main()
     
     for (auto &p : movies) 
     {
-        cout << p.first << ": ";
-        for (int r : p.second) cout << r << " ";
+        cout << p.first <<  endl;
+        cout << "\tRatings: ";
+        for (int r : p.second) 
+        {
+            cout << r << " ";
+            averageRating += r;
+        }
         cout << endl;
+        cout << "\tAverage Rating: " << averageRating / p.second.size() << endl;
     }
 
     return 0;
 }
-
-// Extra Milestone 1
