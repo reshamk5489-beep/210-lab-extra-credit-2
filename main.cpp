52a6cd97ddca6eb43584f86398a0f263876020ba
Resham <resha@MacBook-Air-2.local>
2025-12-09 15:09:32 -0800

    Separating output for each milestone

--- main.cpp
+++ main.cpp
@@ -21,6 +21,41 @@ int main()
 
     file.close();
 
+    // Extra Milestone 1
+    cout << "------Milestone 1------" << endl;
+    cout << "Movies and their ratings:" << endl;
+    for (auto &p : movies) 
+    {
+        cout << "\t" << p.first << ": ";
+        for (int r : p.second) cout << r << " ";
+        cout << endl;
+    }
+
+    cout << endl;
+
+    // Extra Milestone 2
+    cout << "------Milestone 2------" << endl;
+    for (auto &p : movies) 
+    {
+        double averageRating = 0.0;
+        cout << p.first <<  endl;
+        cout << "\tRatings: ";
+        for (int r : p.second) 
+        {
+            cout << r << " ";
+            averageRating += r;
+        }
+
+        cout << endl;
+        cout << "\tAverage Rating: " << averageRating / p.second.size() << endl;
+    }
+
+    cout << endl;
+    cout << "Total movies: " << movies.size() << endl;
+    cout << endl;
+
+    // Extra Milestone 3
+    cout << "------Milestone 3------" << endl;
     for (auto &p : movies) 
     {
         double averageRating = 0.0;
@@ -55,5 +90,3 @@ int main()
 
     return 0;
 }
-
-// Extra Milestone 3
