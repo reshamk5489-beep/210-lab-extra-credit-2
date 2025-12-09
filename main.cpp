7f85761c038fedd941846b7abce02ad3a7ecac78
Resham <resha@MacBook-Air-2.local>
2025-12-09 14:58:13 -0800

    Print movies with highest average rating

--- main.cpp
+++ main.cpp
@@ -10,48 +10,48 @@ int main()
     ifstream file("210-xc2-FA25.txt");
     string title;
     int rating;
-    double maxAverageRating;
+    double maxAverageRating = 0.0;
 
     map<string, vector<int>> movies;
 
     while (file >> title >> rating) 
     {
-        double averageRating = 0.0;
-
-        auto &ratings = movies[title];
-        ratings.push_back(rating);
-        
-        for (int r : ratings) 
-        {
-            averageRating += r;
-        }
-
-        if (averageRating / ratings.size() > maxAverageRating) 
-        {
-            maxAverageRating = averageRating / ratings.size();
-        }
+        movies[title].push_back(rating);
     }
 
     file.close();
-    
+
     for (auto &p : movies) 
     {
         double averageRating = 0.0;
-        cout << p.first <<  endl;
-        cout << "\tRatings: ";
         for (int r : p.second) 
         {
-            cout << r << " ";
             averageRating += r;
         }
-        cout << endl;
-        cout << "\tAverage Rating: " << averageRating / p.second.size() << endl;
+
+        if (averageRating / p.second.size() > maxAverageRating) 
+        {
+            maxAverageRating = averageRating / p.second.size();
+        }
+    }
+    
+    cout << "Highest Average Rating: " << maxAverageRating << endl;
+    cout << "Movies with highest average rating:" << endl;
+
+    for (auto &p : movies) 
+    {
+        double averageRating = 0.0;
+       
+        for (int r : p.second) 
+        {
+            averageRating += r;
+        }
+
+        if (averageRating / p.second.size() >= maxAverageRating) 
+        {
+            cout << "\t" << p.first <<  endl;
+        }
     }
 
-    cout << endl;
-    cout << "Total movies: " << movies.size() << endl;
-
-    cout << "Highest Average Rating: " << maxAverageRating << endl;
-    
     return 0;
 }
