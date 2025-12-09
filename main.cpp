69d46e962a0f3be36dda7292a479a2bc7ca1cb05
Resham <resha@MacBook-Air-2.local>
2025-12-09 14:17:45 -0800

    Using the map to store movies

--- main.cpp
+++ main.cpp
@@ -1,31 +1,31 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <string>
 using namespace std;
-
-class Movie 
-{
-    private:
-        string title;
-        int rating;
-
-    public:
-        Movie(string t, int r) : title(t), rating(r) {}
-
-        string getTitle() const { return title; }
-        int getRating() const { return rating; }
-};
 
 int main() 
 {
     ifstream file("210-xc2-FA25.txt");
-    string line;
+    string title;
+    int rating;
 
-    while (getline(file, line)) 
+    map<string, vector<int>> movies;
+
+    while (file >> title >> rating) 
     {
-        cout << line << endl;
+        movies.insert({title, vector<int>()});
     }
 
     file.close();
+    
+    for (auto &p : movies) 
+    {
+        cout << p.first << ": ";
+        for (int r : p.second) cout << r << " ";
+        cout << endl;
+    }
 
     return 0;
 }
